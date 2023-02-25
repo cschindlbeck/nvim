95c0d49acfce860ad2035b353ea6b59ce9da7207
Chris Schindlbeck <christopher.schindlbeck@iav.de>
2023-02-25 09:13:26 +0100

    Added formatting options

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -10,15 +10,24 @@ return {
   -- local nls = require "null-ls"
   -- local b = nls.builtins
 
+  -- local ls = require('null-ls')
+  -- -- ls.setup({
+  -- debug = true,
+  -- -- Turn on debug for
+  -- :NullLsLog diagnostics_format = "#{m} #{s}[#{c}]",
+  -- sources = {
+  -- ls.builtins.formatting.black.with({ extra_args = {"--line-length=120", "--skip-string-normalization"}, }), …
   require("null-ls").setup({
     sources = {
-      require("null-ls").builtins.formatting.shfmt, -- shell script formatting
       require("null-ls").builtins.diagnostics.shellcheck, -- shell script diagnostics
       require("null-ls").builtins.diagnostics.flake8, -- flake8
-
-
-
-
+      require("null-ls").builtins.formatting.shfmt, -- shell script formatting
+      require("null-ls").builtins.formatting.black.with({
+        extra_args = {
+          "--line-length=120",
+          "--skip-string-normalization"
+        },
+      })
     },
   })
 }
