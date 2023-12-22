2bace38f9f5f1a56b9b9ccd2e365d8d2da8cd969
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2023-12-22 09:23:46 +0100

    Autoformat on save

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -71,9 +71,12 @@ return {
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
-            vim.keymap.set('n', '<space>f', function()
-                vim.lsp.buf.format { async = true }
-            end, opts)
+
+            -- Autoformat
+            vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
+            -- vim.keymap.set('n', '<space>f', function()
+            --     vim.lsp.buf.format { async = true }
+            -- end, opts)
         end
     },
 
