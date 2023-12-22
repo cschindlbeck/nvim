8a784523ae83c9f32a7cf768cff2eeb656a77d4a
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2023-12-22 13:54:03 +0100

    Simplify opts in general lua and added some

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -2,23 +2,25 @@
 -- GLOBAL CONFIGS
 --
 
--- cmd
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-
 -- g
 vim.g.mapleader = " "
 
 -- opt
 local options = {
-  colorcolumn = "120",
-  scrolloff = 15,
-  sidescrolloff = 8,
-  swapfile = false,
+    clipboard = "unnamedplus", -- connection to the system clipboard
+    colorcolumn = "120",       -- show vertical column
+    expandtab = true,          -- enable the use of space in tab
+    ignorecase = true,         -- case insensitive searching
+    number = true,             -- show line numbers
+    relativenumber = false,    -- relative numbers
+    scrolloff = 15,            -- stop scrolling at the top and bottom until the end
+    shiftwidth = 4,            -- number of spaces in indentation
+    sidescrolloff = 8,         -- same for the sides
+    softtabstop = 4,           --
+    swapfile = false,          -- no annoying swap file
+    tabstop = 4,               -- number of spaces for tab
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+    vim.opt[k] = v
 end
