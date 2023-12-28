93329ea82b9ef0fe864cd922af140a77910bf13e
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2023-12-28 15:25:11 +0100

    Add trouble package

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,13 +3,13 @@
 --
 
 -- function _G.set_terminal_keymaps()
---   local opts = { buffer = 0 }
---   vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
---   vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
---   vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
---   vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
---   vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
---   vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+-- local opts = { buffer = 0 }
+-- vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+-- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+-- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+-- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+-- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+-- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
 -- end
 
 -- Remap window navigation
@@ -26,12 +26,12 @@ vim.keymap.set("n", "<S-l>", ":bnext<CR>", {})
 vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Paste and do not overwrite buffer
-vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("x", "<leader>p", '"_dP')
+vim.keymap.set("n", "<leader>p", '"+p')
 
 -- Delete into void buffer
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
 
 -- Move selected lines in visual mode up, down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -41,16 +41,16 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Paste and do not overwrite buffer
-vim.keymap.set("x", "<leader>p", "\"_dP")
-vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("x", "<leader>p", '"_dP')
+vim.keymap.set("n", "<leader>p", '"+p')
 
 -- Delete into void buffer
-vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
 
 -- Move selected lines in visual mode up, down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- Toggleterm and Trouble
--- vim.keymap.set("n", "<leader>tr", [[:TroubleToggle<CR>]])
+-- Trouble
+vim.keymap.set("n", "<leader>tr", [[:TroubleToggle<CR>]])
