4fe23c49b7bc7e0164c7efc7c360c5327ec4a70f
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2023-12-29 13:30:56 +0100

    Enable terminal navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,21 +2,24 @@
 -- KEYMAPS
 --
 
--- function _G.set_terminal_keymaps()
--- local opts = { buffer = 0 }
--- vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
--- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
--- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
--- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
--- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
--- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
--- end
-
 -- Remap window navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", {})
-vim.keymap.set("n", "<C-j>", "<C-w>j", {})
-vim.keymap.set("n", "<C-k>", "<C-w>k", {})
-vim.keymap.set("n", "<C-l>", "<C-w>l", {})
+vim.keymap.set("n", "<C-k>", ":wincmd k<CR>")
+vim.keymap.set("n", "<C-j>", ":wincmd j<CR>")
+vim.keymap.set("n", "<C-h>", ":wincmd h<CR>")
+vim.keymap.set("n", "<C-l>", ":wincmd l<CR>")
+function _G.set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+	vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
+	vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
+	vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
+	vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
+	vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
 -- Remap buffer navigation
 vim.keymap.set("n", "<S-h>", ":bprev<CR>", {})
@@ -33,13 +36,6 @@ vim.keymap.set("n", "<leader>p", '"+p')
 vim.keymap.set("n", "<leader>d", '"_d')
 vim.keymap.set("v", "<leader>d", '"_d')
 
--- Move selected lines in visual mode up, down
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- Yank entire file
-vim.keymap.set("n", "<C-a>", "gg<S-v>G")
-
 -- Paste and do not overwrite buffer
 vim.keymap.set("x", "<leader>p", '"_dP')
 vim.keymap.set("n", "<leader>p", '"+p')
@@ -51,6 +47,9 @@ vim.keymap.set("v", "<leader>d", '"_d')
 -- Move selected lines in visual mode up, down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Select entire file
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
 
 -- Trouble
 vim.keymap.set("n", "<leader>tr", [[:TroubleToggle<CR>]])
