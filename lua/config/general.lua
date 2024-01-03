4fb62917627c47af5cc5c9d3b2470fa27472999a
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2024-01-03 12:27:01 +0100

    Update format general lua

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -8,19 +8,21 @@ vim.g.mapleader = " "
 -- opt
 local options = {
   clipboard = "unnamedplus", -- connection to the system clipboard
-  colorcolumn = "120",      -- show vertical column
-  expandtab = true,         -- enable the use of space in tab
-  ignorecase = true,        -- case insensitive searching
-  number = true,            -- show line numbers
-  relativenumber = false,   -- relative numbers
-  scrolloff = 15,           -- stop scrolling at the top and bottom until the end
-  shiftwidth = 2,           -- number of spaces in indentation
-  sidescrolloff = 8,        -- same for the sides
-  softtabstop = 2,          --
-  swapfile = false,         -- no annoying swap file
-  tabstop = 2,              -- number of spaces for tab
+  colorcolumn = "120",       -- show vertical column
+  expandtab = true,          -- enable the use of space in tab
+  ignorecase = true,         -- case insensitive searching
+  number = true,             -- show line numbers
+  relativenumber = false,    -- relative numbers
+  scrolloff = 15,            -- stop scrolling at the top and bottom until the end
+  shiftwidth = 2,            -- number of spaces in indentation
+  sidescrolloff = 8,         -- same for the sides
+  softtabstop = 2,           --
+  swapfile = false,          -- no annoying swap file
+  tabstop = 2,               -- number of spaces for tab
 }
 
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
+
+
