944e2db0b541eb9ebe69c7127f2ddcf0357a54d9
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2024-01-08 09:16:12 +0100

    Add highlight on yank

--- init.lua
+++ init.lua
@@ -12,6 +12,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("config/autocmds")
 require("config/keymaps")
 require("config/general")
 require("lazy").setup("plugins")
