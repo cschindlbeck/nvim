d1ea70ca40e52cf9663e9e6365d869c0a0767313
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2024-02-06 16:59:38 +0100

    Add git integration

---
+++ lua/plugins/git.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    end
+  },
+  {
+    "tpope/vim-fugitive",
+    config = function()
+      require("gitsigns").setup()
+      vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", {})
+    end
+  },
+}
