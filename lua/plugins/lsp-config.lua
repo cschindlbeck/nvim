7c8c39877ac6b4341abd60d6029e64ca4c7f90a0
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2024-07-08 19:54:25 +0200

    Add filetypes to terraform lsp

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -46,7 +46,10 @@ return {
           },
         },
       })
-      lspconfig.terraformls.setup({ capabilities = capabilities })
+      lspconfig.terraformls.setup({
+        capabilities = capabilities,
+        filetypes = { "terraform", "tf", "hcl" },
+      })
       lspconfig.texlab.setup({ capabilities = capabilities })
       -- lspconfig.yamlls.setup({})
 
