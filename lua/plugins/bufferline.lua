6e84ad9d1a2dfa7a1c395230e3794129ed7194a8
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2024-08-20 10:14:12 +0200

    Highlight selected buffer with white color font

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,4 +3,23 @@ return {
   version = "*",
   dependencies = "nvim-tree/nvim-web-devicons",
   opts = {},
+  config = function()
+    require("bufferline").setup({
+      options = {
+        show_buffer_icons = true,
+        show_buffer_close_icons = true,
+        show_close_icon = true,
+      },
+      highlights = {
+        -- buffer = {
+        --   fg = "#FFFFFF",
+        --   bg = "#011627",
+        -- },
+        buffer_selected = {
+          fg = "#FFFFFF",
+          bf = "#011627",
+        },
+      },
+    })
+  end,
 }
