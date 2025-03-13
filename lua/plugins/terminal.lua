3b1fff96479c7aec71f76bd7decdc253b03425d6
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2025-03-13 13:10:19 +0100

    Toggle term not working yet

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -9,35 +9,43 @@ return {
       },
     },
   },
-  keys = {
-    {
-      "<leader>tt",
-      function()
-        Snacks.terminal.toggle()
-      end,
-      desc = "Toggle terminal",
-    },
-    {
-      "<leader>th",
-      function()
-        Snacks.terminal.open(nil, { win = { position = "bottom" } })
-      end,
-      desc = "Open terminal below",
-    },
-    {
-      "<leader>tv",
-      function()
-        Snacks.terminal.open(nil, { win = { position = "right" } })
-      end,
-      desc = "Open terminal to the right",
-    },
-    {
-      "<leader>tf",
-      function()
-        Snacks.terminal.open(nil, { win = { position = "float" } })
-        -- Snacks.terminal(nil, { win = { position = "float", size = { width = 10, height = 10 } } })
-      end,
-      desc = "Open floating terminal",
-    },
-  },
+  keys = (function()
+    local last_position = "bottom" -- Default position
+
+    local function toggle_last_terminal()
+      Snacks.terminal.toggle(nil, { win = { position = last_position } })
+    end
+
+    return {
+      {
+        "<leader>tt",
+        toggle_last_terminal,
+        desc = "Toggle last opened terminal",
+      },
+      {
+        "<leader>th",
+        function()
+          last_position = "bottom"
+          Snacks.terminal.open(nil, { win = { position = "bottom" } })
+        end,
+        desc = "Open terminal below",
+      },
+      {
+        "<leader>tv",
+        function()
+          last_position = "right"
+          Snacks.terminal.open(nil, { win = { position = "right" } })
+        end,
+        desc = "Open terminal to the right",
+      },
+      {
+        "<leader>tf",
+        function()
+          last_position = "float"
+          Snacks.terminal.open(nil, { win = { position = "float" } })
+        end,
+        desc = "Open floating terminal",
+      },
+    }
+  end)(),
 }
