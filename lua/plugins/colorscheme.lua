fb9d513a778c0ea282949d105bd767a885c7d1b2
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2025-12-21 20:14:06 +0100

    Nord: Remove deprecation

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -17,7 +17,6 @@ local nord = {
   lazy = false,
   priority = 1000,
   config = function()
-    require("nord").setup({})
     vim.cmd.colorscheme("nord")
     local fg = vim.api.nvim_get_hl(0, { name = "Normal" }).fg
     vim.api.nvim_set_hl(0, "SnacksPickerPathHidden", { fg = fg })
