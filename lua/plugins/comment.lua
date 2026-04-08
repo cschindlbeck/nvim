62f83075fb7a9bd9b6abe10ba43202b5c9304224
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2026-04-08 14:58:17 +0200

    merge

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,7 +1,35 @@
 return {
-  "numToStr/Comment.nvim",
+  "echasnovski/mini.comment",
   lazy = false,
-  opts = {
-    -- add any options here
-  },
+  config = function()
+    require("mini.comment").setup({
+      options = {
+        -- Function to compute custom 'commentstring' (optional)
+        custom_commentstring = nil,
+
+        -- Whether to ignore blank lines in actions and textobject
+        ignore_blank_line = false,
+
+        -- Whether to recognize as comment only lines without indent
+        start_of_line = false,
+
+        -- Whether to force single space inner padding for comment parts
+        pad_comment_parts = true,
+      },
+
+      mappings = {
+        -- Default mappings for commenting
+        comment = "gc",
+        comment_line = "gcc",
+        comment_visual = "gc",
+        textobject = "gc",
+      },
+
+      hooks = {
+        -- Hooks that run pre and post commenting actions
+        pre = function() end,
+        post = function() end,
+      },
+    })
+  end,
 }
