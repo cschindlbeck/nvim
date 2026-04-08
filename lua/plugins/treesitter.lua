0b52629968253974fd516cf133f24ad6ec1a5f7a
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2026-04-08 20:08:50 +0200

    treesitter migration

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,9 +2,23 @@ return {
   "nvim-treesitter/nvim-treesitter",
   branch = "main",
   build = ":TSUpdate",
-  opts = {
-    auto_install = true,
-    highlight = { enable = true },
-    indent = { enable = true },
-  },
+  init = function()
+    -- Ensure parsers and configure highlighting/indentation
+    local ensureInstalled = { "lua", "python", "typescript" }
+    local alreadyInstalled = require("nvim-treesitter.config").get_installed()
+    local parsersToInstall = vim
+      .iter(ensureInstalled)
+      :filter(function(parser)
+        return not vim.tbl_contains(alreadyInstalled, parser)
+      end)
+      :totable()
+    require("nvim-treesitter").install(parsersToInstall)
+
+    vim.api.nvim_create_autocmd("FileType", {
+      callback = function()
+        pcall(vim.treesitter.start)
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
