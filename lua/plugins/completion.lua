27abb03a9ecea04e87a1b2eb2e06ca519c5c89fe
Chris Schindlbeck <chris.schindlbeck@gmail.com>
2026-04-21 16:38:48 +0200

    feat: replace nvim-cmp with blink.cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,94 +1,33 @@
 return {
   {
-    "hrsh7th/cmp-nvim-lsp",
-    lazy = true,
-    event = "InsertEnter",
-    config = true,
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    lazy = true,
-    event = "InsertEnter",
-    dependencies = {
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-buffer",
-      "onsails/lspkind.nvim",
-      -- "quangnguyen30192/cmp-nvim-ultisnips",
-      "rafamadriz/friendly-snippets",
-      "saadparwaiz1/cmp_luasnip",
+    "saghen/blink.cmp",
+    version = "1.*",
+    opts = {
+      keymap = {
+        preset = "default",
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide", "fallback" },
+        ["<CR>"] = { "accept", "fallback" },
+      },
+      appearance = {
+        nerd_font_variant = "mono",
+      },
+      completion = {
+        documentation = {
+          auto_show = true,
+          window = { border = "rounded" },
+        },
+        menu = {
+          border = "rounded",
+        },
+      },
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer" },
+      },
+      fuzzy = { implementation = "prefer_rust_with_warning" },
     },
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    lazy = true,
-    event = "InsertEnter",
-    config = function()
-      local cmp = require("cmp")
-      local lspkind = require("lspkind")
-      cmp.setup({
-        preselect = cmp.PreselectMode.None,
-        completion = { completeopt = "noselect" },
-
-        snippet = {
-          -- REQUIRED - you must specify a snippet engine
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "copilot", group_index = 1 },
-          { name = "buffer", group_index = 2 },
-          { name = "luasnip", group_index = 2 },
-          { name = "nvim_lsp", group_index = 2 },
-          { name = "path", group_index = 2 },
-        }),
-        sorting = {
-          comparators = {
-            cmp.config.compare.offset,
-            cmp.config.compare.exact,
-            cmp.config.compare.score,
-            cmp.config.compare.kind,
-            cmp.config.compare.sort_text,
-            cmp.config.compare.length,
-            cmp.config.compare.order,
-          },
-        },
-
-        formatting = {
-          format = lspkind.cmp_format({
-            mode = "symbol", -- show only symbol annotations
-            maxwidth = 50, -- prevent the popup from showing more than provided characters
-            -- (e.g 50 will not show more than 50 characters)
-            -- can also be a function to dynamically calculate max width such as
-            -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
-            ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char
-            -- instead (must define maxwidth first)
-            -- The function below will be called before any actual modifications from lspkind
-            -- so that you can provide more controls on popup customization.
-            -- (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
-            -- before = function (entry, vim_item)
-            --   ...
-            --   return vim_item
-            -- end
-            symbol_map = { Copilot = "" },
-          }),
-        },
-      })
-    end,
+    opts_extend = { "sources.default" },
   },
 }
